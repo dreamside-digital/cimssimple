872a581fb6a4666e9f37c5592a4f43e16387ccf9
Sharon <sharon.peishan.kennedy@gmail.com>
2018-04-16 19:56:48 -0400

    use color palette

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,30 +7,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { renderToString } from 'react-dom/server'
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import pink from 'material-ui/colors/pink';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#932F6D',
-      main: '#932F6D',
-      dark: '#932F6D',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#13505B',
-      main: '#13505B',
-      dark: '#13505B',
-      contrastText: '#fff',
-    },
-    error: pink
-  },
-  typography: {
-    fontFamily: "\"Open Sans\", \"Helvetica\", \"Arial\", sans-serif"
-  }
-});
-
 
 import createStore from './src/redux/store'
 
@@ -39,9 +15,7 @@ exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
 
   const ConnectedBody = () => (
       <Provider store={store}>
-        <MuiThemeProvider theme={theme}>
-          {bodyComponent}
-        </MuiThemeProvider>
+        {bodyComponent}
       </Provider>
   )
   replaceBodyHTMLString(renderToString(<ConnectedBody/>))
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import pink from 'material-ui/colors/pink';
+import cyan from 'material-ui/colors/cyan';
+
 import './index.css'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#13505B',
+      main: '#13505B',
+      dark: '#13505B',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#932F6D',
+      main: '#932F6D',
+      dark: '#932F6D',
+      contrastText: '#fff',
+    },
+    error: pink
+  },
+  typography: {
+    fontFamily: "\"Open Sans\", \"Helvetica\", \"Arial\", sans-serif"
+  }
+});
 
 const Layout = ({ children, data }) => (
   <div>
@@ -14,7 +39,9 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <div>
-      {children()}
+      <MuiThemeProvider theme={theme}>
+        {children()}
+      </MuiThemeProvider>
     </div>
   </div>
 )
