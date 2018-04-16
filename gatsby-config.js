2dcde58691c1ef5a973576623e3932fa85306c97
Sharon <sharon.peishan.kennedy@gmail.com>
2018-04-16 02:32:41 -0400

    add manifest and offline plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'CIMS Simple Form Generator',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "CIMSsimple: Planning, Docketing, and Evaluation Tool",
+        short_name: "CIMSsimple",
+        start_url: "/",
+        background_color: "#F0F2EF",
+        theme_color: "#13505B",
+        display: "minimal-ui",
+        icon: "static/icon.png" // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
 }
