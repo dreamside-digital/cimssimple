9f4d335938c2596e1ef27ffa5a39e593505cee53
Sharon <sharon.peishan.kennedy@gmail.com>
2018-05-28 10:22:48 -0400

    add url-search-params polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,8 @@
  */
 
 import 'babel-polyfill';
+import 'url-search-params-polyfill';
+
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
